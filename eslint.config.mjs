bd8d7162024ac537a1cecc77057fec904006804e
Luís Vitor <luis.sg.vitor@gmail.com>
2024-12-12 13:02:53 -0300

    config: scripts and lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,12 +7,14 @@ import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   {
-    files: ['**/*.{ts}'],
-    ignores: ['**/node_modules/**', '**/.serverless/**'],
+    files: ['**/*.ts'],
   },
   { languageOptions: { globals: globals.node } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   prettier,
   eslintPluginPrettierRecommended,
+  {
+    ignores: ['**/node_modules/**', '.serverless/**/*.js'],
+  },
 ];
